8dde5f74122c0912617f34268987825d36134ab5
WilliamIson <77437958+WilliamIson@users.noreply.github.com>
2021-01-28 10:32:23 +0000

    enemies

--- main.ts
+++ main.ts
@@ -68,17 +68,17 @@ for (let index = 0; index < 3; index++) {
     mySprite2.follow(mySprite3)
 }
 forever(function () {
-    if (mySprite2.tileKindAt(TileDirection.Left, sprites.castle.tilePath5)) {
+    if (mySprite2.isHittingTile(CollisionDirection.Left)) {
         mySprite2.setVelocity(50, 0)
     } else {
         mySprite2.follow(mySprite3)
     }
-    if (mySprite2.tileKindAt(TileDirection.Top, sprites.castle.tilePath5)) {
+    if (mySprite2.isHittingTile(CollisionDirection.Top)) {
         mySprite2.setVelocity(100, 0)
     } else {
-        mySprite2.follow(mySprite2)
+        mySprite2.follow(mySprite3)
     }
-    if (mySprite2.tileKindAt(TileDirection.Right, sprites.castle.tilePath5)) {
+    if (mySprite2.isHittingTile(CollisionDirection.Right)) {
         mySprite2.setVelocity(-50, 0)
     } else {
         mySprite2.follow(mySprite3)
